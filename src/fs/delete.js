4cd986377dc0c614d57a045319a08c2bea434ea9
Omelchuk Maxim <omelchukmv@solit.by>
2023-04-06 14:35:11 +0300

    feat: done fs 4

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,11 @@
+import fs from "fs";
+
 const remove = async () => {
-    // Write your code here 
+  const path = "./files/fileToRemove.txt";
+  const callback = (e) => {
+    if (e) throw new Error("FS operation failed");
+  };
+  fs.unlink(path, callback);
 };
 
 await remove();
