56063d6bc1145101d62a42961ec58a157c753aca
Omelchuk Maxim <omelchukmv@solit.by>
2023-04-06 14:48:40 +0300

    feat: done fs 6

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,11 @@
+import fs from "fs";
+
 const read = async () => {
-    // Write your code here 
+  const path = "./files/fileToRead.txt";
+  fs.readFile(path, (err, data) => {
+    if (err) throw new Error("FS operation failed");
+    console.log(data.toString());
+  });
 };
 
 await read();
