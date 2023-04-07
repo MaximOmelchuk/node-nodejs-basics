f12059f425182a75054b971eb1221d7108ae061c
Omelchuk Maxim <omelchukmv@solit.by>
2023-04-07 17:13:35 +0300

    feat: threads done 2

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,21 +1,10 @@
-import { Worker, workerData, parentPort } from "worker_threads";
+import { workerData, parentPort } from "worker_threads";
 
 // n should be received from main thread
 const nthFibonacci = (n) =>
   n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-  //   const input = process.argv[2];
-  //   const path = "./nthFibonacci.js";
-  //   const worker = new Worker(path, { workerData: { value: input } });
-  // This function sends result of nthFibonacci computations to main thread
-  //   worker.on("message", (result) => {
-  //     console.log(result);
-  //   });
-  //   worker.on("exit", (code) => {
-  //     if (code !== 0) throw new Error(`Worker stopped with exit code ${code}`);
-  //     else console.log("Worker stopped " + code);
-  //   });
   parentPort.postMessage(nthFibonacci(workerData.value));
 };
 
