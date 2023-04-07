bdc7618bb1e0aedc382a9bd91629d80df24d23db
Omelchuk Maxim <omelchukmv@solit.by>
2023-04-07 17:54:23 +0300

    feat: start cp 1

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,11 @@
+import { spawn, exec } from "child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = spawn("node script", args, { cwd: "./files" });
+  child.stdout.on("data", (data) => {
+    console.log(data);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2"]);
