979cc1a6a2c3d5e67a634429996aaa371637f622
Omelchuk Maxim <omelchukmv@solit.by>
2023-04-07 13:54:30 +0300

    feat: done streams 1

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,10 @@
+// import stream from "stream";
+import fs from "fs";
 const read = async () => {
-    // Write your code here 
+  const path = "./files/fileToRead.txt";
+  const stream = fs.createReadStream(path);
+  stream.on("error", (error) => console.log(error));
+  stream.on("data", (chunk) => process.stdout.write(chunk));
 };
 
 await read();
